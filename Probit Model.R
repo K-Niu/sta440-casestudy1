ecb7a81eec07dd51d6d4e1ee03f43ef9646d9c6c
Kelvin Niu <kelvinniu23@gmail.com>
2016-09-10 22:54:44 -0400

    Added non-working probit JAGS code

---
+++ Probit Model.R
@@ -0,0 +1,64 @@
+library(R2jags)
+library(R2WinBUGS)
+
+load('probit_data.Rdata')
+
+data.list <- list(I = dataset[,"i"], 
+                  U = dataset[,"u"], 
+                  V = dataset[,"v"], 
+                  Y = dataset[,"y"], 
+                  n = dim(dataset),
+                  num.patients = 114,
+                  num.regions = 68)
+
+model <- "
+model {
+  ##
+  #Likelihood
+  ##
+  for(k in 1:n) {
+    p[k] <- pnorm(alpha[I[k]] + lambda[I[k]]*abs(z[U[k]] - z[V[k]]), 0, 1)
+    Y[k] ~ dbinom(p[k], 1)
+  }
+
+  ##
+  #Priors for z's
+  ##
+  for(u in 1:num.regions) {
+    z[u] ~ dnorm(0, 1)
+  }
+
+  ##
+  #Priors for alpha's
+  ##
+  sd.a ~ dt(0, 0.1, 1) T(0,)
+  phi.a <- pow(sd.a, -2)
+  for(i in 1:num.patients) {
+    alpha[i] ~ dnorm(0, phi.a)
+  }
+
+  ##
+  #Priors for lambda's
+  ##
+  sd.l ~ dt(0, 0.1, 1) T(0,)
+  phi.l <- pow(sd.l, -2)
+  for(i in 1:num.patients) {
+    lambda[i] ~ dnorm(0, phi.l)
+  }
+
+}
+"
+
+inits <- function() {
+  z <- rep(0, data.list$num.regions)
+  alpha <- rep(0, data.list$num.patients)
+  lambda <- rep(0, data.list$num.patients)
+  sd.a <- 0.1
+  sd.l <- 0.1
+  return(list(z = z, alpha = alpha, lambda = lambda, sd.a = sd.a, sd.l = sd.l))
+}
+
+parameters <- c("z", "alpha", "lambda", "sd.a", "sd.l")
+
+game.sim <- jags(data.list, inits = inits, parameters, model.file = textConnection(model), n.iter = 5000)
+game.bugs <- as.mcmc(game.sim$BUGSoutput$sims.matrix)
